1310d92ce0f28bd88466086b8ded8e05a6273802
BenjaminCHEN <chenyijun0902@gmail.com>
2016-12-01 10:36:48 +0800

    我就看看这东西怎么用的

--- src/t.java
+++ src/t.java
@@ -4,7 +4,7 @@ public class t {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, Rushhour");
-
+		System.out.println("我就看看这东西怎么用的");
 	}
 
 }
